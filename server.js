ad5d109e38b7a7f9ea1411fd2ad1bc9ac2b0a260
Zach White <zechnwhite@gmail.com>
2020-02-22 18:00:48 -0800

    deploy 3

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
     log('Homepage visited.')
     res.sendFile(__dirname + '/index.html');
 })
+app.get('/hello', (req, res) => {
+    log('Homepage visited.')
+    res.send('Hello... yeah that\'s all this route does. Sorry about that.');
+})
+app.get('/goat', (req, res) => {
+    log('Homepage visited.')
+    res.send('Tom Brady, obviously.');
+})
 
 const port = process.env.PORT || 3000;
 
